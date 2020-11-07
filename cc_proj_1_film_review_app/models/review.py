acb8503885cb9051b4af9f43e9540adcf709eb99
Ian F <ian.fiddeman@gmail.com>
2020-11-07 23:11:23 +0000

    classes set up

---
+++ cc_proj_1_film_review_app/models/review.py
@@ -0,0 +1,8 @@
+class Review:
+
+    def __init__(self, id = None, customer_id, film_id, customer_rating, comment):
+        self.id = id
+        self.customer_id = customer_id
+        self.film_id = film_id
+        self.customer_rating = customer_rating
+        self.comment = comment
